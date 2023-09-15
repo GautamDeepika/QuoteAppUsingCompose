ea5d0b2a6725413ed3e7aade23a6cad67dcbdcb1
GautamDeepika <97622775+GautamDeepika@users.noreply.github.com>
2023-09-15 20:34:09 +0530

    -new project commited for compose tutorial

--- app/src/main/java/com/example/appusingcompose/MainActivity.kt
+++ app/src/main/java/com/example/appusingcompose/MainActivity.kt
@@ -3,32 +3,41 @@ package com.example.appusingcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.appusingcompose.screens.QuoteDetailScreen
 import com.example.appusingcompose.screens.QuoteListScreen
 import com.example.appusingcompose.ui.theme.AppUsingComposeTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         CoroutineScope(Dispatchers.IO).launch {
+            delay(8000)
             DataManager.loadAssetsFromFile(applicationContext)
         }
         setContent {
@@ -59,7 +68,7 @@ private fun AppTheme() {
 //    LocalConfiguration.current.colorMode - to acess theme related things
 //     LocalContext.current  -  to acess context
 //     LocalContext.current.applicationContext  -  to acess application context
-    var theme = remember { mutableStateOf(  false) }
+    var theme = remember { mutableStateOf(false) }
 
     AppUsingComposeTheme(theme.value) {
         Surface(
@@ -97,10 +106,11 @@ private fun App() {
             modifier = Modifier
                 .fillMaxSize(1f)
         ) {
-            Text(
-                text = "Loading....",
-                style = MaterialTheme.typography.headlineLarge
-            )
+//            Text(
+//                text = "Loading....",
+//                style = MaterialTheme.typography.headlineLarge
+//            )
+            loader()
         }
     }
 
@@ -120,6 +130,31 @@ private fun App() {
 //        Greeting("Android")
 //    }
 //}
+
+@Composable
+fun loader() {
+    val degree = produceState(initialValue = 0) {
+        while (true) {
+            delay(16)
+            value = (value + 30) % 360
+        }
+    }
+
+    Box(contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize(1f),
+        content = {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Image(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "Loading",
+                    modifier = Modifier
+                        .size(60.dp)
+                        .rotate(degree.value.toFloat())
+                )
+                Text(text = "Loading")
+            }
+        })
+}
 
 
 enum class Pages {
